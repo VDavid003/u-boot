c243a4d43d07c77b559084eef309d68477528de1
VDavid003 <virag.david003@gmail.com>
2023-07-09 15:42:19 +0200

    reboot support

--- board/samsung/jackpotlte/jackpotlte.c
+++ board/samsung/jackpotlte/jackpotlte.c
@@ -9,5 +9,8 @@ int exynos_init(void)
 {
 	// This makes S-Boot's framebuffer continue on working.
 	*((int*)0x14860070) = 0x1281;
+
+	// S-Boot checks this register and powers off if this isn't set correctly when resetting.
+	*((int*)0x11c80808) = 0x12345678;
 	return 0;
 }
