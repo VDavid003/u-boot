142c44bf8c481b2b8469fc9345d19f0ef655eb24
VDavid003 <virag.david003@gmail.com>
2023-07-16 12:48:58 +0200

    gpio keys

--- include/configs/exynos7885-common.h
+++ include/configs/exynos7885-common.h
@@ -54,7 +54,7 @@
 
 #ifndef EXYNOS_DEVICE_SETTINGS
 #define EXYNOS_DEVICE_SETTINGS \
-	"stdin=serial\0" \
+	"stdin=serial,button-kbd\0" \
 	"stdout=serial,vidconsole\0" \
 	"stderr=serial,vidconsole\0"
 #endif
